977489b7005270a1d41676a37638d989b629ef6c
Sanidhya Shrimali <sanidhyashrimali2810@gmail.com>
2026-04-21 00:15:55 +0530

    Update 4_user_info.py

--- pages/4_user_info.py
+++ pages/4_user_info.py
@@ -18,9 +18,9 @@ else:
     result = response.json()
     
     if "message" in result:
-      col1,col2 = st.columns(2)
-      col3,col4 = st.columns(2)
-      col5,col6 = st.columns(2)
+      col1,col2 = st.columns(2,gap="xxsmall")
+      col3,col4 = st.columns(2,gap="xxsmall")
+      col5,col6 = st.columns(2,gap="xxsmall")
       with col1:
         st.subheader("Name")
       with col2:
