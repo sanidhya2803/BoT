8e45425d5e1cc10ea634713d094d14c679f75f3b
Sanidhya Shrimali <sanidhyashrimali2810@gmail.com>
2026-04-20 10:56:15 +0530

    Add files via upload

---
+++ pages/2_Login.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+
+st.title("User Login")
+
+email = st.text_input("Enter your email",placeholder="Eg. user123@gmail.com")
+password = st.text_input("Enter your password",placeholder="Eg. Password@1234")
+
+col1,col2 = st.columns([2,10],gap="xxsmall")
+valid = False
+if email and password:
+    valid = True
+
+flag = False
+with col1:
+    if st.button("Submit",disabled=not valid):
+
+        response = requests.post(
+            url="http://127.0.0.1:8000/login_user",
+            json = {"email":email,"password":password}
+        )
+
+        result = response.json()
+        flag = True
+
+with col2:
+    if st.button("Don't have account?"):
+        st.switch_page("pages/1_Register.py")
+
+if flag:
+    if "user" in result:    
+        st.success(f"Hello {result["user"]}")
+
+        st.session_state.user = result["user"]
+        st.session_state.active_user = True
+    elif "error" in result:
+        st.error(result["error"])
+    elif "detail" in result:
+        st.error(result["detail"][0]["msg"])
