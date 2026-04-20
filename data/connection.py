9b08abcee1e74a68d129eeb83b9e4b2d1d3cb72f
Sanidhya Shrimali <sanidhyashrimali2810@gmail.com>
2026-04-20 13:39:12 +0530

    Update connection.py

--- data/connection.py
+++ data/connection.py
@@ -1,14 +1,16 @@
 import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_conncetion():
-
     conn = pymysql.connect(
-        host = "bov6ijok5h5wiyrgatvt-mysql.services.clever-cloud.com",
-        user = "utz3yxtpj7g9p7bk",
-        password = "JZwLTangw8caeg77YXYW",
-        database = "bov6ijok5h5wiyrgatvt",
-        port = 3306,
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306)),
         cursorclass=pymysql.cursors.DictCursor
     )
-
     return conn
