355a8f575a44eeca11cde59c5e622ae991633731
Sanidhya Shrimali <sanidhyashrimali2810@gmail.com>
2026-04-21 15:18:37 +0530

    Update schemas.py

--- model/schemas.py
+++ model/schemas.py
@@ -11,6 +11,10 @@ class Login_User(BaseModel):
     email:str=Field(...,pattern="^[a-zA-Z0-9._%#+-]+@gmail\\.com$")
     password:str=Field(...,min_length=8,max_length=16)
 
+class Update_User(BaseModel):
+    name:Optinal[str]=None
+    age:Optional[str]=None
+    
 class UserChat(BaseModel):
     message:str
 
